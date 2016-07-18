a2f0712456aed5d56ce1afe4836c795ccf1c77cd
czarinamae <czarinadejesus07@gmail.com>
2016-07-18 11:45:41 +0800

    Add files via upload

---
+++ czarinadejesus.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<windows.h>
+using namespace std;
+
+int main()
+{
+	
+	cout<<"\t\t\t\t\t\\_________________________"<<endl;
+	cout<<"\t\t\t\t\t | -------------------------\\"<<endl;
+	cout<<"\t\t\t\t\t |   R O C K E T              ||\\"<<endl;
+	cout<<"\t\t\t\t\t | -------------------------- ||/"<<endl;
+	cout<<"\t\t\t\t\t |__________________________//"<<endl;
+	cout<<"\t\t\t\t\t//"<<endl;
+	cout<<"\n"<<endl;
+	cout<<"\n"<<endl;
+	cout<<"\n"<<endl;
+	cout<<"\n"<<endl;
+	cout<<"\n"<<endl;
+	
+	Sleep(300);
+	system("cls");
+	
+	cout<<"\t\t\t\t\t\t\\_________________________"<<endl;
+	cout<<"\t\t\t\t\t\t | -------------------------\\"<<endl;
+	cout<<"\t\t\t\t\t\t |   R O C K E T              ||\\"<<endl;
+	cout<<"\t\t\t\t\t\t | -------------------------- ||/"<<endl;
+	cout<<"\t\t\t\t\t\t |__________________________//"<<endl;
+	cout<<"\t\t\t\t\t\t//"<<endl;
+	cout<<"\n"<<endl;
+	cout<<"\n"<<endl;
+	cout<<"\n"<<endl;
+	cout<<"\n"<<endl;
+	cout<<"\n"<<endl;
+	
+	Sleep(300);
+	system("cls");
+	
+	system("pause");
+	return 0;
+}
