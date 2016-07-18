bb517d2954a01004e8700454340b31d8ed194642
johnpatrickdeguzman <johnpatrickdeguzman123@gmail.com>
2016-07-18 11:41:18 +0800

    Add files via upload

---
+++ patrickdeguzman.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+#include<windows.h>
+using namespace std;
+
+int main()
+{
+	cout<<"       /\\"<<endl;
+	cout<<"      /  \\"<<endl;
+	cout<<"     /____\\"<<endl;
+	cout<<"    /      \\"<<endl;
+	cout<<"   ||__R__||"<<endl;
+	cout<<"   ||__O__||"<<endl;
+	cout<<"   ||__C__||"<<endl;
+	cout<<"   ||++K++||"<<endl;
+	cout<<"   ||__E__||"<<endl;
+	cout<<"   ||__T__||"<<endl;
+	cout<<"   |^^^^^^^|"<<endl;
+	cout<<"  /  _____  \\"<<endl; 
+	cout<<" /_/       \\_\\\\"<<endl;
+	cout<<"\n"<<endl;
+	cout<<"\n"<<endl;
+	
+	system("cls");
+	
+	Sleep (300);
+	
+	cout<<"\t\\_________________________"<<endl;
+	cout<<"\t | -------------------------\\"<<endl;
+	cout<<"\t |   R O C K E T              ||\\"<<endl;
+	cout<<"\t | -------------------------- ||/"<<endl;
+	cout<<"\t |__________________________//"<<endl;
+	cout<<"\t//"<<endl;
+	cout<<"\n"<<endl;
+	cout<<"\n"<<endl;
+	cout<<"\n"<<endl;
+	cout<<"\n"<<endl;
+	cout<<"\n"<<endl;
+	
+	Sleep(300);
+	system("cls");
+	
+	cout<<"\t\t\t\\_________________________"<<endl;
+	cout<<"\t\t\t | -------------------------\\"<<endl;
+	cout<<"\t\t\t |   R O C K E T              ||\\"<<endl;
+	cout<<"\t\t\t | -------------------------- ||/"<<endl;
+	cout<<"\t\t\t |__________________________//"<<endl;
+	cout<<"\t\t\t//"<<endl;
+	cout<<"\n"<<endl;
+	cout<<"\n"<<endl;
+	cout<<"\n"<<endl;
+	cout<<"\n"<<endl;
+	cout<<"\n"<<endl;
+	
+	Sleep(300);
+	system("cls");
+	
+	cout<<"\t\t\t\t\\_________________________"<<endl;
+	cout<<"\t\t\t\t | -------------------------\\"<<endl;
+	cout<<"\t\t\t\t |   R O C K E T              ||\\"<<endl;
+	cout<<"\t\t\t\t | -------------------------- ||/"<<endl;
+	cout<<"\t\t\t\t |__________________________//"<<endl;
+	cout<<"\t\t\t\t//"<<endl;
+	cout<<"\n"<<endl;
+	cout<<"\n"<<endl;
+	cout<<"\n"<<endl;
+	cout<<"\n"<<endl;
+	cout<<"\n"<<endl;
+		
+	Sleep(500);
+	system("cls");
+	
+	system("pause");
+	return 0;
+}
