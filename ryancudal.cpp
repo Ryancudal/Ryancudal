7ffcd94f56c498900156fadea5b509064fab9314
Ryancudal <ryancudal420@gmail.com>
2016-07-18 11:38:25 +0800

    Add files via upload

---
+++ ryancudal.cpp
@@ -0,0 +1,127 @@
+#include<iostream>
+#include<windows.h>
+using namespace std;
+
+int main()
+{
+	int a;
+	for(a=1;a<2;a=1+1)
+	
+	
+	cout<<"\n"<<endl;
+	cout<<"\n"<<endl;
+	cout<<"\n"<<endl;
+	cout<<"\n"<<endl;
+	cout<<"\n"<<endl;
+	cout<<"       /\\"<<endl;
+	cout<<"      /  \\"<<endl;
+	cout<<"     /____\\"<<endl;
+	cout<<"    /      \\"<<endl;
+	cout<<"   ||__R__||"<<endl;
+	cout<<"   ||__O__||"<<endl;
+	cout<<"   ||__C__||"<<endl;
+	cout<<"   ||++K++||"<<endl;
+	cout<<"   ||__E__||"<<endl;
+	cout<<"   ||__T__||"<<endl;
+	cout<<"   |^^^^^^^|"<<endl;
+	cout<<"  /  _____  \\"<<endl; 
+	cout<<" /_/       \\_\\\\"<<endl;
+	cout<<"    ^^^^^^ "<<endl;
+	
+	Sleep(300);
+	
+	system("cls");
+	
+	cout<<"\n"<<endl;
+	cout<<"\n"<<endl;
+	cout<<"\n"<<endl;
+	cout<<"\n"<<endl;
+	cout<<"       /\\"<<endl;
+	cout<<"      /  \\"<<endl;
+	cout<<"     /____\\"<<endl;
+	cout<<"    /      \\"<<endl;
+	cout<<"   ||__R__||"<<endl;
+	cout<<"   ||__O__||"<<endl;
+	cout<<"   ||__C__||"<<endl;
+	cout<<"   ||++K++||"<<endl;
+	cout<<"   ||__E__||"<<endl;
+	cout<<"   ||__T__||"<<endl;
+	cout<<"   |^^^^^^^|"<<endl;
+	cout<<"  /  _____  \\"<<endl; 
+	cout<<" /_/       \\_\\\\"<<endl;
+	cout<<"    ^^^^^^ "<<endl;
+	cout<<"\n"<<endl;
+	
+	Sleep(300);
+	
+	system("cls");
+	
+
+	cout<<"\n"<<endl;
+	cout<<"\n"<<endl;
+	cout<<"\n"<<endl;
+	cout<<"       /\\"<<endl;
+	cout<<"      /  \\"<<endl;
+	cout<<"     /____\\"<<endl;
+	cout<<"    /      \\"<<endl;
+	cout<<"   ||__R__||"<<endl;
+	cout<<"   ||__O__||"<<endl;
+	cout<<"   ||__C__||"<<endl;
+	cout<<"   ||++K++||"<<endl;
+	cout<<"   ||__E__||"<<endl;
+	cout<<"   ||__T__||"<<endl;
+	cout<<"   |^^^^^^^|"<<endl;
+	cout<<"  /  _____  \\"<<endl; 
+	cout<<" /_/       \\_\\\\"<<endl;
+	cout<<"\n"<<endl;
+	cout<<"\n"<<endl;
+	
+	Sleep(300);	
+	
+	system("cls");
+	
+	cout<<"\n"<<endl;
+	cout<<"\n"<<endl;
+	cout<<"       /\\"<<endl;
+	cout<<"      /  \\"<<endl;
+	cout<<"     /____\\"<<endl;
+	cout<<"    /      \\"<<endl;
+	cout<<"   ||__R__||"<<endl;
+	cout<<"   ||__O__||"<<endl;
+	cout<<"   ||__C__||"<<endl;
+	cout<<"   ||++K++||"<<endl;
+	cout<<"   ||__E__||"<<endl;
+	cout<<"   ||__T__||"<<endl;
+	cout<<"   |^^^^^^^|"<<endl;
+	cout<<"  /  _____  \\"<<endl; 
+	cout<<" /_/       \\_\\\\"<<endl;
+	cout<<"\n"<<endl;
+	cout<<"\n"<<endl;
+	cout<<"\n"<<endl;
+	
+	Sleep(300);
+	
+	system("cls");
+	
+	cout<<"       /\\"<<endl;
+	cout<<"      /  \\"<<endl;
+	cout<<"     /____\\"<<endl;
+	cout<<"    /      \\"<<endl;
+	cout<<"   ||__R__||"<<endl;
+	cout<<"   ||__O__||"<<endl;
+	cout<<"   ||__C__||"<<endl;
+	cout<<"   ||++K++||"<<endl;
+	cout<<"   ||__E__||"<<endl;
+	cout<<"   ||__T__||"<<endl;
+	cout<<"   |^^^^^^^|"<<endl;
+	cout<<"  /  _____  \\"<<endl; 
+	cout<<" /_/       \\_\\\\"<<endl;
+	cout<<"\n"<<endl;
+	cout<<"\n"<<endl;
+	
+	system("cls");
+	Sleep (300);	
+	
+	system("pause");
+	return 0;
+}
